4a07072acd6bdcad42e4f59036fc64868e29ecd6
Lara Sanders <larasanders@gmail.com>
2018-07-22 21:51:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,13 +7,11 @@ const reps = 'learn-co-curriculum/javascript-fetch-lab'
 
 function getIssues() {
 //   const token = ' da4f675e128aa66a149793db2bead57d6c6490a6';
-//
-// fetch(`${reps}/issues`, {
-//   headers: {
-//     Authorization: `token ${getToken()}`
-//   }
-// }).then(res => res.json()).then(json => console.log(json));
-// }
+
+fetch(`${reps}/issues`, {
+
+}).then(res => res.json()).then(json => console.log(json));
+}
 }
 
 function showIssues(json) {
