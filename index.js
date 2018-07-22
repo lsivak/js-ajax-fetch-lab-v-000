476c4cc56401915dcddd6fb7fa52c2354d7146ae
Lara Sanders <larasanders@gmail.com>
2018-07-22 19:10:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const userName = 'lsivak'
-const baseApi = 'https://api.github.com/'
-const fork = `${username}/learn-co-curriculum/javascript-fetch-lab'`
-
 function getIssues() {
 }
 
@@ -14,35 +10,18 @@ function createIssue() {
 function showResults(json) {
 }
 
-Repo.prototype.template = function(){
-  var template = `<h3>Forked Successfully!</h3><a href="${this.url}"> ${this.url}</a>`
-  return template;
-};
-
-function Repo(attributes){
-  this.url = attributes.url;
-}
-
 function forkRepo() {
-      const token = ' da4f675e128aa66a149793db2bead57d6c6490a6';
+    const token = ' da4f675e128aa66a149793db2bead57d6c6490a6';
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  fetch('https://api.github.com/repos/lsivak/learn-co-curriculum/javascript-fetch-lab', {
-    method: 'POST'
+  fetch('learn-co-curriculum/javascript-fetch-lab', {
     headers: {
       Authorization: `token ${token}`
     }
-  }).then(resp => {
-    let repo = new Repo(resp);
-    showForkedRepo(repo);
-  })
-}
-
-function showForkedRepo(repo) {
-  $('#results').append(repo)
+  }).then(res => res.json()).then(json => console.log(json));
 }
 
 function getToken() {
-
+  const token = ' da4f675e128aa66a149793db2bead57d6c6490a6';
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
   return ''
