a6bd793e1051f4bdf0ee4d38632ca8206b4d9a4a
Lara Sanders <larasanders@gmail.com>
2018-07-22 20:21:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const userName = 'lsivak'
 const baseApi = 'https://api.github.com/'
 const fork = `${userName}/javascript-fetch-lab`
 const reps = 'learn-co-curriculum/javascript-fetch-lab'
-debugger
 function getIssues() {
 }
 
@@ -16,8 +15,8 @@ function showResults(json) {
 }
 
 function forkRepo() {
+  debugger
     const token = ' da4f675e128aa66a149793db2bead57d6c6490a6';
-debugger
 
   fetch(`https://api.github.com/repos/${reps}/forks`, {
     method: 'POST',
