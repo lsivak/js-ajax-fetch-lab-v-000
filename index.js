80256bd980d135ef6271354cbfa878ed34086193
Lara Sanders <larasanders@gmail.com>
2018-07-22 19:24:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,12 +12,20 @@ function showResults(json) {
 
 function forkRepo() {
     const token = ' da4f675e128aa66a149793db2bead57d6c6490a6';
+    const postData = {
+  body: 'Great stuff'
+};
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   fetch(`https://api.github.com/repos/${repo}`, {
+    method: 'POST',
+    body: JSON.stringify(showForkedRepo(repo)),
     headers: {
       Authorization: `token ${getToken()}`
     }
   }).then(res => res.json()).then(json => console.log(json));
+}
+function showForkedRepo(repo) {
+  $('#results').append(repo.template())
 }
 
 function getToken() {
