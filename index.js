1b703eb39ed116733a570dc87a8f491054673f85
Lara Sanders <larasanders@gmail.com>
2018-07-22 21:12:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,42 +1,83 @@
 const userName = 'lsivak'
 const baseApi = 'https://api.github.com/'
 const fork = `${userName}/javascript-fetch-lab`
-const reps = 'learn-co-curriculum/javascript-fetch-lab'
-  let repo = `${baseApi}repos/${reps}/forks`
-function getIssues() {
+
+//Issue and Repo objects and templates
+
+function Issue(attributes){
+  this.title = attributes.title;
+  this.body = attributes.body;
+  this.url = attributes.url;
 }
 
-function showIssues(json) {
+function Repo(attributes){
+  this.url = attributes.url;
 }
+
+Issue.prototype.template = function(){
+   var template = `<li>Title: <a href="${this.url}">${this.title} </a><span> | Body: ${this.body}</span></li>`
+   return template;
+};
+
+Repo.prototype.template = function(){
+  var template = `<h3>Forked Successfully!</h3><a href="${this.url}"> ${this.url}</a>`
+  return template;
+};
+
+//Create an issue through the Github API
 
 function createIssue() {
+  const issueTitle = document.getElementById('title').value
+  const issueBody = document.getElementById('body').value
+  const postData = { title: issueTitle, body: issueBody }
+  fetch(`${baseApi}repos/${fork}/issues`, {
+    method: 'post',
+    headers: {
+      'Authorization': `token ${getToken()}`
+    },
+    body: JSON.stringify(postData)
+  }).then(resp => getIssues())
 }
 
-function showResults(json) {
+//Fetch all issues through the Github API and display / append to the DOM
+
+function getIssues(data) {
+  fetch(`${baseApi}repos/${fork}/issues`).
+    then(resp => {
+      resp.json().then( data => {
+        for (let i = 0; i < data.length; i++){
+          displayIssue(new Issue(data[i]));
+        }
+      } )
+    })
 }
+
+function displayIssue(issue) {
+  $('#issues').append(issue.template())
+}
+
+//Fetch and show repo info
+
 
 function forkRepo() {
-    const token = ' da4f675e128aa66a149793db2bead57d6c6490a6';
+  const repo = 'learn-co-curriculum/javascript-fetch-lab'
 
-  fetch(`https://api.github.com/repos/${reps}/forks`, {
+  fetch(`${baseApi}repos/${repo}/forks`, {
     method: 'post',
-
     headers: {
-      Authorization: `token ${getToken()}`
+      'Authorization': `token ${getToken()}`
     }
-  }).then(res => res.json(showForkedRepo(repo))).then(json => console.log(json));
+  }).then(resp => {
+    let repo = new Repo(resp);
+    showForkedRepo(repo);
+  })
 }
-function Repo(repo){
-  return `<h3>Forked Successfully!<a href="${repo}"> ${repo}</a>`
 
-};
 function showForkedRepo(repo) {
-$("#results").html(Repo(repo))
+  $('#results').append(repo.template())
 }
+
 
 function getToken() {
-
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
   return ''
 }
