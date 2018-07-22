4727d473e2b14b284612fec99c7705d1df9be9ee
Lara Sanders <larasanders@gmail.com>
2018-07-22 18:52:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function showResults(json) {
 }
 
 function forkRepo() {
-    // const token = ' da4f675e128aa66a149793db2bead57d6c6490a6';
+    const token = ' da4f675e128aa66a149793db2bead57d6c6490a6';
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   fetch(`${baseApi}repos/${repo}/forks`, {
     method: 'POST'
@@ -30,6 +30,7 @@ function forkRepo() {
 
 
 function getToken() {
+      const token = ' da4f675e128aa66a149793db2bead57d6c6490a6';
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
   return ''
