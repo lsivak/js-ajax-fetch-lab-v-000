a52b58a876681ff59d0bdba90608493639400a5b
Lara Sanders <larasanders@gmail.com>
2018-07-22 20:18:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function forkRepo() {
     const token = ' da4f675e128aa66a149793db2bead57d6c6490a6';
     const postData = {
       debugger
-  body: `${showForkedRepo(repo)}`
+  body: showForkedRepo(repo)
 };
 
   fetch(`https://api.github.com/repos/${reps}/forks`, {
